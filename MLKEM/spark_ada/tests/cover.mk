6a1c6ebf516979c50f5aaed0d4f155cfad1b2af7
Rod Chapman <rodchap@amazon.com>
2026-08-06 12:38:16 +0100

    Adjust pathnames to libkeccak for new dir structure
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

--- MLKEM/spark_ada/tests/cover.mk
+++ MLKEM/spark_ada/tests/cover.mk
@@ -18,13 +18,13 @@ tkats: tkats.gpr tkats.adb ../lib/libmlkem.a
 	@echo "--- Building tkats ---"
 	gprbuild -Ptkats -XMLKEM_HOSTOS=$(HOSTOS) -XMLKEM_COVERAGE=enabled
 
-../lib/libmlkem.a: ../src/*.ads ../src/*.adb ../libkeccak/lib/generic_none/libkeccak.a
+../lib/libmlkem.a: ../src/*.ads ../src/*.adb ../../../libkeccak/lib/generic_none/libkeccak.a
 	@echo "--- Building LibMLKEM ---"
 	cd ..; gprbuild -Pmlkem -XMLKEM_COVERAGE=enabled -XMLKEM_BUILD_MODE=debug -XMLKEM_RUNTIME_CHECKS=disabled -XMLKEM_CONTRACTS=disabled
 
-../libkeccak/lib/generic_none/libkeccak.a: ../libkeccak/src/common/*.ads ../libkeccak/src/common/*.adb
+../../../libkeccak/lib/generic_none/libkeccak.a: ../../../libkeccak/src/common/*.ads ../../../libkeccak/src/common/*.adb
 	@echo "--- Building libkeccak ---"
-	cd ../libkeccak; alr build
+	cd ../../../libkeccak; alr build
 
 clean:
 	gprclean -Ptkats
--- MLKEM/spark_ada/tests/fast.mk
+++ MLKEM/spark_ada/tests/fast.mk
@@ -1,5 +1,5 @@
 include Makefile.common
 
-../lib/libmlkem.a: ../src/*.ads ../src/*.adb ../libkeccak/lib/generic_none/libkeccak.a
+../lib/libmlkem.a: ../src/*.ads ../src/*.adb ../../../libkeccak/lib/generic_none/libkeccak.a
 	@echo "--- Building LibMLKEM ---"
 	cd ..; gprclean -Pmlkem; gprbuild -Pmlkem -XMLKEM_BUILD_MODE=O3 -XMLKEM_RUNTIME_CHECKS=disabled -XMLKEM_CONTRACTS=disabled
